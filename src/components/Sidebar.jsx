2ec7aedf99e745ab6f5e6002194ce473ef31791e
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-19 15:26:55 +0200

    Refactor Sidebar component: updated styles for MenuContainer, added Flex component, and modified Logo component with new styles and layout.

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -4,23 +4,41 @@ import { HomeRounded, CloseRounded } from "@mui/icons-material";
 
 const MenuContainer = styled.div`
   flex: 0.5;
-  flex-direction: colum;
+  flex-direction: column;
   height: 100vh;
   display: flex;
   background: ${({ theme }) => theme.bg};
   color: ${({ theme }) => theme.text_primary};
 `;
-const Logo = styled.div``;
+const Flex = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+`;
+
+const Logo = styled.div`
+  color: ${({ theme }) => theme.primary};
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 6px;
+  font-size: 20px;
+  font-weight: bold;
+  margin: 16px 0px;
+`;
 const Elements = styled.div``;
 const NavTex = styled.div``;
 
 const Sidebar = () => {
   return (
     <MenuContainer>
-      <Logo>Cinematic</Logo>
-      <close>
-        <CloseRounded />
-      </close>
+      <Flex>
+        <Logo>Cinematic</Logo>
+        <close>
+          <CloseRounded />
+        </close>
+      </Flex>
       <Elements>
         <HomeRounded />
         <NavTex>Dashboard</NavTex>

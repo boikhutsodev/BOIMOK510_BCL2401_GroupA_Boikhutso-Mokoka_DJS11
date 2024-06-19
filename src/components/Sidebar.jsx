fa4bf79785617322132fe9c108a90879687ed098
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-19 14:37:21 +0200

    Sidebar layout

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import styled from "styled-components";
+import { HomeRounded, CloseRounded } from "@mui/icons-material";
+
+const MenuContainer = styled.div``;
+const Logo = styled.div``;
+const Elements = styled.div``;
+const NavTex = styled.div``;
 
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  return (
+    <MenuContainer>
+      <Logo></Logo>
+      <close>
+        <CloseRounded />
+      </close>
+      <Elements>
+        <HomeRounded />
+        <NavTex>Dashboard</NavTex>
+      </Elements>
+    </MenuContainer>
+  );
 };
 
 export default Sidebar;

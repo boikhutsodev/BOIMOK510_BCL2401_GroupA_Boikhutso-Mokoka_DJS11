1697d61bfc56e3a7efe244943f10557a5a988bd9
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-19 14:45:00 +0200

    Updated Sidebar component: added styles to MenuContainer, added text to Logo component

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -2,7 +2,14 @@ import React from "react";
 import styled from "styled-components";
 import { HomeRounded, CloseRounded } from "@mui/icons-material";
 
-const MenuContainer = styled.div``;
+const MenuContainer = styled.div`
+  flex: 0.5;
+  flex-direction: colum;
+  height: 100vh;
+  display: flex;
+  background: ${({ theme }) => theme.bg};
+  color: ${({ theme }) => theme.text_primary};
+`;
 const Logo = styled.div``;
 const Elements = styled.div``;
 const NavTex = styled.div``;
@@ -10,7 +17,7 @@ const NavTex = styled.div``;
 const Sidebar = () => {
   return (
     <MenuContainer>
-      <Logo></Logo>
+      <Logo>Cinematic</Logo>
       <close>
         <CloseRounded />
       </close>

6b42e07f706ecb22f4eb9c3ab39b06230af17af8
Boikhutso Mokoka <boikhuso.m@gmail.com>
2024-06-21 01:27:12 +0200

    Updated NavBar component: changed background color, added ButtonDiv with PersonRounded icon and 'Login' text.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,38 @@
 import React from "react";
 import styled from "styled-components";
 import MenuIcon from "@mui/icons-material/Menu";
+import { PersonRounded } from "@mui/icons-material";
 
 const NavBarDiv = styled.div`
   display: flex;
   justify-content: space-between;
-  width: 100%;
   padding: 16px 40px;
   align-items: center;
   gap: 30px;
-  background: ${({ theme }) => theme.bg};
+  background: ${({ theme }) => theme.bgLight};
   color: ${({ theme }) => theme.text_primary};
-  border-radius: 16px;
   box-shadow: 0 4px 30px rgba(0, 0, 0, 0.1);
   backdrop-filter: blur(5.7px);
   -webkit-backdrop-filter: blur(5.7px);
+`;
+
+const ButtonDiv = styled.div`
+  font-size: 14px;
+  cursor: pointer;
+  max-width: 70px;
+  align-items: center;
+  color: ${({ theme }) => theme.primary};
+  border: 1px solid ${({ theme }) => theme.primary};
 `;
 
 const NavBar = () => {
   return (
     <NavBarDiv>
       <MenuIcon />
+      <ButtonDiv>
+        <PersonRounded />
+        Login
+      </ButtonDiv>
     </NavBarDiv>
   );
 };
